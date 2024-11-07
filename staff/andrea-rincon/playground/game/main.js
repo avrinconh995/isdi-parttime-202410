29e39ea0e90a2d5ea5ce063d39437ee007288940
andrea-rincon <andrearincon995@gmail.com>
2024-11-07 21:11:46 +0100

    add dorraymon es6 and folder game #4

---
+++ staff/andrea-rincon/playground/game/main.js
@@ -0,0 +1,16 @@
+var root = document.body.querySelector('#root')
+
+var bluey = document.createElement('div')
+root.appendChild(bluey)
+var blueyImage = document.createElement('img')
+blueyImage.src = 'images/bluey-body.png'
+bluey.appendChild(blueyImage)
+
+
+
+//var bingo = document.createElement('img')
+//bingo.src = 'images/bingo-run.png'
+
+
+
+
