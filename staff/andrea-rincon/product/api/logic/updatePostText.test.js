4e844671bdc7b767d2fde7522ca435c24a2e58eb
andrea-rincon <andrearincon995@gmail.com>
2025-03-10 18:35:54 +0100

    update code updatePostText #37

--- staff/andrea-rincon/product/api/logic/updatePostText.test.js
+++ staff/andrea-rincon/product/api/logic/updatePostText.test.js
@@ -4,7 +4,7 @@ import updatePostText from './updatePostText.js'
 mongoose.connect('mongodb://localhost:27017/product')
     .then(() => {
         try {
-            updatePostText('67c448ca832e2e9afdac3ce3', '67c47384c2d9dfa3dd780709', 'new text')
+            updatePostText('67c448ca832e2e9afdac3ce3', '67c47384c2d9dfa3dd780709', 'new 2')
                 .then(result => console.log('post text updated', result))
                 .catch(error => console.error(error))
         } catch (error) {
